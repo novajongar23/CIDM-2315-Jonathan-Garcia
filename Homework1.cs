989393fc8a97eb05ff8570116786a76b9e37a2d7
novajongar23 <111917060+novajongar23@users.noreply.github.com>
2022-08-29 16:09:45 -0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+       double x = 2.5;
+       double y = 3.3;
+       double z = 4*x*x+3*y;
+       Console.WriteLine($"x = {x}; y = {y}");
+       Console.WriteLine($"The value of z is: {z}");
+    }
+}
