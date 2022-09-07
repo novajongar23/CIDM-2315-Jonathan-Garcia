a725917cd01913b1370195934d99c49234deec3d
novajongar23 <111917060+novajongar23@users.noreply.github.com>
2022-09-07 16:18:04 -0500

    Add files via upload

---
+++ Homework 2.cs
@@ -0,0 +1,101 @@
+﻿namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        /*
+        A:4
+        B:3
+        C:2
+        D:1
+        F:0
+        other - "Wrong Letter Grade"
+        */
+
+        
+
+    //    Console.WriteLine("Please Input Letter Grade");
+    //     string grade = Console.ReadLine();
+
+    //     if(grade == "A"){
+    //         Console.WriteLine("GPA Point: 4");
+    //     }
+    //     else if(grade == "B"){
+    //             Console.WriteLine("GPA Point: 3");
+    //             }
+    //             else if(grade == "C"){
+    //                 Console.WriteLine("GPA Point: 2");
+    //                 }
+    //                 else if(grade == "D"){
+    //                     Console.WriteLine("GPA Point: 1");
+    //                 }
+    //                 else if(grade == "F"){
+    //                     Console.WriteLine("GPA Point: 0");
+    //                 }
+    //                 else{
+    //                     Console.WriteLine("Wrong Letter Grade");
+    //                 }
+
+
+                    
+
+                
+
+                // int num1, num2, num3;
+
+                // Console.WriteLine("Please input first number: ");
+                // num1 = Convert.ToInt16(Console.ReadLine());
+
+                // Console.WriteLine("Please input the second number: ");
+                // num2 = Convert.ToInt16(Console.ReadLine());
+
+                // Console.WriteLine("Please input the third number: ");
+                // num3 = Convert.ToInt16(Console.ReadLine());
+
+                // if(num1 <= num2 && num1 <= num3){
+                //     Console.WriteLine("Smallest number is:" + num1);
+                // }
+                // if(num2 <= num1 && num2 <= num3){
+                //     Console.WriteLine("Smallest number is:" + num2);
+                // }
+                // if(num3 <= num1 && num3 <= num1){
+                //     Console.WriteLine("Smallest number is:" + num3);
+                // }
+
+
+                // int year;
+                // Console.WriteLine("Please in put a year: ");
+                // year = Convert.ToInt16(Console.ReadLine());
+
+                // if(((year % 4 == 0) &&(year % 100 != 0) || (year % 400 == 0)))
+                // {
+                //     Console.WriteLine(year + "is a leap year");
+                // }
+                // else
+                // {
+                //     Console.WriteLine(year + "is not a leap year");
+                // }
+
+
+
+
+
+
+
+
+                }
+            }
+        
+
+
+        
+
+
+
+        
+
+
+    
+
